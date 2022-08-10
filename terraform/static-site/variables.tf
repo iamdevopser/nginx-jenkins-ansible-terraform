0ae53d93636790c48d6e2c8eb98fb89346a9abc8
EC2 Default User <ec2-user@ip-172-31-92-143.ec2.internal>
2022-08-10 09:23:40 +0000

    myproject-03

--- terraform/static-site/variables.tf
+++ terraform/static-site/variables.tf
@@ -1,15 +1,19 @@
 variable "profile" {
+    profile = "iamdevopser"
     description = "The profile used to auth to AWS"
 }
 
 variable "region" {
+    region = "us-east-1"
     description= "The region our instance will be in (i.e. us-east-1)"
 }
 
 variable "name" {
+    name = "site"
     description= "The name of the instance we are creating"
 }
 
 variable "group" {
+    group = "web"
     description= "the name of the group we will be using for Ansible purposes"
 }
